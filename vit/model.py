56de064a2fa273ff8d85371129804736145f4efa
PumeTu <pumetuchinda@gmail.com>
2022-11-04 15:52:52 -0400

    add building blocks

---
+++ vit/model.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Attention(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+class MLP(nn.Module):
+    def __init__(self,):
+        pass
+
+    def forward(self,):
+        pass
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+class Encoder(nn.Module):
+    def __init__(self,):
+        pass
+
+    def forward(self):
+        pass
+
+class Decoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+class VisionTransformer(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
