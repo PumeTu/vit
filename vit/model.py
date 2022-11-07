ed8456480f24fbd171519e5ab3cd0801b4a2a022
PumeTu <pumetuchinda@gmail.com>
2022-11-07 14:40:50 -0500

    add embedding and attention block

--- vit/model.py
+++ vit/model.py
@@ -1,43 +1,65 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Attention(nn.Module):
-    def __init__(self):
-        pass
+class PatchifyEmbed(nn.Module):
+    '''
+    Split an image into patches and embed the patches
+    Args:
+        img_size (int): size of the image 
+        patch_size (int): size of the patch 
+        in_channels (int): number of input channels
+        embed_dim (int): embedding dimension
+    '''
+    def __init__(self, img_size, patch_size, in_channels, embed_dim):
+        super().__init__()
+        self.n_patches = (img_size // patch_size) ** 2
+        self.patchify = nn.Conv2d(in_channels, embed_dim, kernel_size=patch_size, stride=patch_size)
+        
+    def forward(self, x):
+        x = self.patchify(x)
+        x = x.flatten(start_dim=-2, end_dim=-1)
+        x = x.transpose(1, 2)
+        return x
 
-    def forward(self):
-        pass
+class SelfAttention(nn.Module):
+    '''
+    Vanilla Self Attention Block
+    '''
+    def __init__(self, dim, n_head=12, qkv_bias=True, atten_pdrop=0., proj_pdrop=0.):
+        self.n_head = n_head
+        self.dim = dim
+        #queries, key, value projection
+        self.qkv = nn.Linear(dim, dim*3, bias=qkv_bias)
+        #output projection
+        self.proj = nn.Linear(dim, dim)
 
-class MLP(nn.Module):
-    def __init__(self,):
-        pass
+        #Regularization
+        self.attn_drop = nn.Dropout(atten_pdrop)
+        self.proj_drop = nn.Dropout(proj_pdrop)
 
-    def forward(self,):
-        pass
+    def forward(self, x):
+        B, T, C = x.size() #batch size, sequence length, embedding dimensionality
 
-class PositionalEmbedding(nn.Module):
-    def __init__(self):
-        pass
+        q, k ,v  = self.qkv(x).split(self.dim, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
 
-    def forward(self):
-        pass
+        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        att = self.attn_dropout(att)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
 
-class Encoder(nn.Module):
-    def __init__(self,):
-        pass
+        # output projection
+        y = self.resid_dropout(self.c_proj(y))
+        return y
 
-    def forward(self):
-        pass
-
-class Decoder(nn.Module):
-    def __init__(self):
-        pass
-
-    def forward(self):
-        pass
-
-class VisionTransformer(nn.Module):
+class ViT(nn.Module):
     def __init__(self):
         pass
 
