955b3d1fdfdaa79de38475f41dd92b237d6727eb
PumeTu <pumetuchinda@gmail.com>
2022-11-10 15:12:02 -0500

    added some more building blocks

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# VIT
-PyTorch reimplementation of Google Vision Transformers (An Image Is Worth 16x16 Words: Transformers For Image Recognition At Scale)
+## Vision Transformers: Comprehension and Implementation
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-red.svg)](#python)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
+
+#### Author:
+* [Pume Tuchinda]
+
+### Overview
+This repository contains an implmentation of the Vision Transformer, as desribed in the ICLR2021 Paper An Image is Worth 16x16 Words: Transformers For Image Recognition at Scale (https://arxiv.org/pdf/2010.11929.pdf) in PyTorch. The original repository of the code (implemented in JAX) is available at: https://github.com/google-research/vision_transformer.
+
--- vit/model.py
+++ vit/model.py
@@ -59,6 +59,31 @@ class SelfAttention(nn.Module):
         y = self.resid_dropout(self.c_proj(y))
         return y
 
+class MLP(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features, p=0.):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = nn.GELU()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(p)
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+
+        return x
+
+class Block(nn.Module):
+    """Transformer Block"""
+    def __init__(self, dim, n_heads, mlp_ratio, qkv_bias, p=0., atten_p=0.):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(dim, eps=1e-6)
+        self.attn = SelfAttention(dim,n_head=n_heads, qkv_bias=qkv_bias, atten_pdrop=atten_p, proj_pdrop=p)
+        self.norm2 = nn.LayerNorm(dim, eps=1e-6)
+        self.mlp = MLP(in_features=dim, hidden_features=hidden_features)
 class ViT(nn.Module):
     def __init__(self):
         pass
